a46fe535b048244ad2928f5b93c3f54177459c95
vicframe <victor@frameworkagency.io>
2025-08-21 14:08:43 -0400

    Update Twilio4.js

--- backend/Twilio4.js
+++ backend/Twilio4.js
@@ -6,15 +6,8 @@ const VoiceResponse = require('twilio/lib/twiml/VoiceResponse')
             
 
 class Twilio{
-    phoneNumber = '+16175044426'
-    phonenNumberSid = 'PN8ea1699149f0acd7a1cc63ea4eecf92d'
-    tokenSid = 'SK4411a33df3fe8ce0516b644d080f91a7'
-    tokenSecret = '4yK4LfHvoV24N4onYbOauJykXy4TPITT'
-    accountSid = 'AC4c3a89e56afcb829253b16848592cbea'
-    verify = 'VA296574a1cec0b611282d9e7f30b6894d'
-    outgoingApplicationSid = 'APae8310637ab70bff9a1e4c7c2bf12018'
+ 
 
-    authToken = 'ac57125790f08b78ab7546146dc755e1'
     client;
 
     constructor(){
