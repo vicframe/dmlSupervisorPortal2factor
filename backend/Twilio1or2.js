ea227e711a0d4b1f2aeb42991fa80d4cda97cee2
vicframe <victor@frameworkagency.io>
2025-08-21 14:08:22 -0400

    Update Twilio1or2.js

--- backend/Twilio1or2.js
+++ backend/Twilio1or2.js
@@ -4,15 +4,7 @@ const twilio=require('twilio')
             
 
 class Twilio{
-    phoneNumber = '+16175044426'
-    phonenNumberSid = 'PN8ea1699149f0acd7a1cc63ea4eecf92d'
-    tokenSid = 'SK4411a33df3fe8ce0516b644d080f91a7'
-    tokenSecret = '4yK4LfHvoV24N4onYbOauJykXy4TPITT'
-    accountSid = 'AC4c3a89e56afcb829253b16848592cbea'
-    verify = 'VA296574a1cec0b611282d9e7f30b6894d'
-
-    authToken = 'ac57125790f08b78ab7546146dc755e1'
-    client;
+   
 
     constructor(){
         this.client=twilio(this.tokenSid, this.tokenSecret, {
